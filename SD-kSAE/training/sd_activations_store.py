49cb0e5fbabc75496a31fc77caabf25731e910e3
revelio-diffusion <revelio25@proton.me>
2024-11-15 18:57:15 -0500

    Update sd_activations_store.py

--- SD-kSAE/training/sd_activations_store.py
+++ SD-kSAE/training/sd_activations_store.py
@@ -12,16 +12,10 @@ class CustomFeatureDataset(Dataset):
             feature_dir (str): Path to the directory containing feature .npy files.
             label_dir (str): Path to the directory containing label .npy files.
         """
-            # Create a DataLoader with shuffling and batching
         self.activations = []
         self.feature_dir = os.path.join(dataset_dir, layer_dir)
         
         # # Load the cached activations and labels
-        # activation_files = sorted([f for f in os.listdir(self.feature_dir)], key=lambda x: int(re.search(r'image_(\d+)_features.npy', x).group(1)))
-        # for activation_file in activation_files:
-        #     self.activations.append(torch.tensor(np.load(os.path.join(self.feature_dir, activation_file))).clone().detach().mean((-1,-2)))
-        
-
         activation_files = sorted(
             [f for f in os.listdir(self.feature_dir) if f.endswith('.pt')],
             key=lambda x: tuple(map(int, re.search(r'image_(\d+)_(\d+)_features.pt', x).groups())) if re.search(r'image_(\d+)_(\d+)_features.pt', x) else (float('inf'), float('inf'))  # Assign inf if no match
@@ -62,14 +56,10 @@ class SDActivationsStore:
         self, cfg: SDSAERunnerConfig, create_dataloader: bool = True, train=True,
     ):
         self.cfg = cfg
-        
         if self.cfg.use_cached_activations:
-
             self.feature_dataset = CustomFeatureDataset(self.cfg.model_name, self.cfg.layer_name)
             self.feature_loader = DataLoader(self.feature_dataset, batch_size=self.cfg.batch_size, shuffle=True)
             self.loader_iter = iter(self.feature_loader)
-
-    
     
     def next_batch(self):
         """
